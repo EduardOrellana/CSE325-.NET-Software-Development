de709302d4d9ecd5c94c27e461bf36be0b3dee8b
EduardOrellana <eduardorellana13@gmail.com>
2025-09-19 23:23:32 -0600

    W3

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06c4027b7dbd2cdeecd9230fd35be6cdd29ab092")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52889d93d5ffad7f31d518d7142a3163720b9d87")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
